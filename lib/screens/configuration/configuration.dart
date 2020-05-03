22c24638b79b20fc7cae2f32272ec565cdeced83
Ben Dobos <bendobos1@gmail.com>
2020-05-03 02:20:44 +0200

    chore: code formatting

--- lib/screens/configuration/configuration.dart
+++ lib/screens/configuration/configuration.dart
@@ -12,14 +12,21 @@ class Configuration extends StatefulWidget {
 }
 
 class _ConfigurationState extends State<Configuration> {
-  final RoundedLoadingButtonController _grocyTestBtnController = new RoundedLoadingButtonController();
-  final RoundedLoadingButtonController _bbuddyBtnController = new RoundedLoadingButtonController();
+  final RoundedLoadingButtonController _grocyTestBtnController =
+      new RoundedLoadingButtonController();
+  final RoundedLoadingButtonController _bbuddyBtnController =
+      new RoundedLoadingButtonController();
 
   _persistSetting(String key, String value) {
     PrefService.setString(key, value);
   }
 
-  void _testApiConnection(BuildContext context, String baseUrl, String apiKey, String authHeader, RoundedLoadingButtonController buttonController) async {
+  void _testApiConnection(
+      BuildContext context,
+      String baseUrl,
+      String apiKey,
+      String authHeader,
+      RoundedLoadingButtonController buttonController) async {
     final url = '$baseUrl/api/system/info';
 
     try {
@@ -27,13 +34,16 @@ class _ConfigurationState extends State<Configuration> {
 
       if (response.statusCode == 200) {
         buttonController.success();
-        Scaffold.of(context).showSnackBar(SnackBar(content: Text('Success! HTTP Status ${response.statusCode.toString()}.')));
+        Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(
+                'Success! HTTP Status ${response.statusCode.toString()}.')));
       } else {
         buttonController.error();
-        Scaffold.of(context).showSnackBar(SnackBar(content: Text('HTTP Error ${response.statusCode.toString()}.')));
+        Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text('HTTP Error ${response.statusCode.toString()}.')));
         Timer(Duration(seconds: 3), () => buttonController.reset());
       }
-    } catch(error) {
+    } catch (error) {
       throw error;
     }
   }
@@ -45,84 +55,70 @@ class _ConfigurationState extends State<Configuration> {
         appBar: AppBar(
           title: Center(child: Text("Configuration")),
         ),
-        body:
-          Builder(
-            builder: (BuildContext context) {
-              return PreferencePage([
-                Container(
-                    padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-                    child: Text('Grocy Settings', style: TextStyle(fontWeight: FontWeight.bold))
+        body: Builder(builder: (BuildContext context) {
+          return PreferencePage([
+            Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                child: Text('Grocy Settings',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            TextFieldPreference(
+              'Grocy Server URL',
+              'grocy_server_url',
+              onChange: (String value) {
+                _persistSetting('grocy_server_url', value);
+              },
+            ),
+            TextFieldPreference(
+              'Grocy API Key',
+              'grocy_api_key',
+              onChange: (String value) {
+                _persistSetting('grocy_api_key', value);
+              },
+            ),
+            RoundedLoadingButton(
+                child: Text(
+                  'Test Connection',
+                  style: TextStyle(color: Colors.white),
                 ),
-                TextFieldPreference(
-                  'Grocy Server URL',
-                  'grocy_server_url',
-                  onChange: (String value) {
-                    _persistSetting('grocy_server_url', value);
-                  },
-                ),
-                TextFieldPreference(
-                  'Grocy API Key',
-                  'grocy_api_key',
-                  onChange: (String value) {
-                    _persistSetting('grocy_api_key', value);
-                  },
-                ),
-                RoundedLoadingButton(
-                  child: Text(
-                    'Test Connection',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  controller: _grocyTestBtnController,
-                  color: Colors.deepPurple,
-                  onPressed: () => _testApiConnection(
+                controller: _grocyTestBtnController,
+                color: Colors.deepPurple,
+                onPressed: () => _testApiConnection(
                     context,
                     PrefService.getString('grocy_server_url'),
                     PrefService.getString('grocy_api_key'),
                     'GROCY-API-KEY',
-                    _grocyTestBtnController
-                  )
-                ),
-                Divider(
-                  color: Colors.deepPurple,
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  child: Text('Barcode Buddy Settings', style: TextStyle(fontWeight: FontWeight.bold))
-                ),
-                TextFieldPreference(
-                  'Barcode Buddy URL',
-                  'bbuddy_server_url',
-                  onChange: (String value) {
-                    _persistSetting('bbuddy_server_url', value);
-                  }
-                ),
-                TextFieldPreference(
-                  'Barcode Buddy API Key',
-                  'bbuddy_api_key',
-                  onChange: (String value) {
-                    _persistSetting('bbuddy_api_key', value);
-                  }
-                ),
-                RoundedLoadingButton(
-                  child: Text(
-                    'Test Connection',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  controller: _bbuddyBtnController,
-                  color: Colors.deepPurple,
-                  onPressed: () => _testApiConnection(
-                      context,
-                      PrefService.getString('bbuddy_server_url'),
-                      PrefService.getString('bbuddy_api_key'),
-                      'BBUDDY-API-KEY',
-                      _bbuddyBtnController
-                  ),
-                )
-              ]);
-            }
-          )
-
-    );
+                    _grocyTestBtnController)),
+            Divider(
+              color: Colors.deepPurple,
+            ),
+            Container(
+                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: Text('Barcode Buddy Settings',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            TextFieldPreference('Barcode Buddy URL', 'bbuddy_server_url',
+                onChange: (String value) {
+              _persistSetting('bbuddy_server_url', value);
+            }),
+            TextFieldPreference('Barcode Buddy API Key', 'bbuddy_api_key',
+                onChange: (String value) {
+              _persistSetting('bbuddy_api_key', value);
+            }),
+            RoundedLoadingButton(
+              child: Text(
+                'Test Connection',
+                style: TextStyle(color: Colors.white),
+              ),
+              controller: _bbuddyBtnController,
+              color: Colors.deepPurple,
+              onPressed: () => _testApiConnection(
+                  context,
+                  PrefService.getString('bbuddy_server_url'),
+                  PrefService.getString('bbuddy_api_key'),
+                  'BBUDDY-API-KEY',
+                  _bbuddyBtnController),
+            )
+          ]);
+        }));
   }
 
   void onButtonPress() {
